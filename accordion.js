ea6139e066d12e9fce376947cd0c456c241247f0
VihanVla <111562310+VihanVla@users.noreply.github.com>
2023-04-18 12:36:23 +0430

    uncommenting

--- accordion.js
+++ accordion.js
@@ -1,17 +1,7 @@
-// let accordion = document.querySelector(".accordion")
-
-// Array.from(accordion.children).forEach(wrapper => {
-
-//   wrapper.querySelector("span").addEventListener("click", e => {
-//     let span = e.target
-//     let wrapper = span.parentElement
-//     wrapper.classList.toggle("show")
-//   })
-// })
-
 let accordion = document.querySelector(".accordion")
 
 Array.from(accordion.children).forEach(wrapper => {
+
   wrapper.querySelector("span").addEventListener("click", e => {
     let span = e.target
     let wrapper = span.parentElement
