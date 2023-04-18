0f01b34013d2c4284d3190fb8cc3c11cc77cd9ee
VihanVla <111562310+VihanVla@users.noreply.github.com>
2023-04-18 12:32:47 +0430

    Initial commit

---
+++ accordion.js
@@ -0,0 +1,20 @@
+// let accordion = document.querySelector(".accordion")
+
+// Array.from(accordion.children).forEach(wrapper => {
+
+//   wrapper.querySelector("span").addEventListener("click", e => {
+//     let span = e.target
+//     let wrapper = span.parentElement
+//     wrapper.classList.toggle("show")
+//   })
+// })
+
+let accordion = document.querySelector(".accordion")
+
+Array.from(accordion.children).forEach(wrapper => {
+  wrapper.querySelector("span").addEventListener("click", e => {
+    let span = e.target
+    let wrapper = span.parentElement
+    wrapper.classList.toggle("show")
+  })
+})
