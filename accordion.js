9a676c4c7c4309c0658f3592895f41cbf69766a3
VihanVla <111562310+VihanVla@users.noreply.github.com>
2023-04-19 10:27:48 +0430

    css changes

--- accordion.js
+++ accordion.js
@@ -6,5 +6,11 @@ Array.from(accordion.children).forEach(wrapper => {
     let span = e.target
     let wrapper = span.parentElement
     wrapper.classList.toggle("show")
+
+    Array.from(accordion.children).forEach(w => {
+      if (w != wrapper){
+        w.classList.remove("show")
+      }
+    } )
   })
 })
